0f464f206a76c31495f3a54c6d9e3b21ebf532fa
Kalakood <>
2023-01-22 19:36:04 +0200

    *

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -1,8 +1,10 @@
 # pages/urls.py
 from django.urls import path
 from .views import homePageView
-from .views import HView
+from .views import HView, AboutPageView
 urlpatterns = [
     path("e/", homePageView, name="home_old"),
+    path("cpc/", AboutPageView.as_view(), name="about"),
+    path("about2/", AboutPageView.as_view(), name="about"),
     path("", HView.as_view(), name="home"),
 ]
