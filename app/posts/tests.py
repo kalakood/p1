27c2e52269c2cbb76f7c4048c356037d7390b658
Kalakood <>
2023-01-23 21:14:20 +0200

    .

--- app/posts/tests.py
+++ app/posts/tests.py
@@ -1,3 +1,31 @@
+import sys
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+class PostTetst(TestCase):
+  @classmethod
+  def setUpTestData(cls):
+    cls.post = Post.objects.create(text="This is a test!")
+    #sys.stderr.write(repr(object_to_print) + '\n')
+    sys.stderr.write("setup done!\n")
+
+  def test_model_content(self):
+    self.assertEqual(self.post.text, "This is a test!")
+
+  def test_url_exists_at_correct_location(self):
+    response = self.client.get(reverse("listika"))
+    self.assertEqual(response.status_code, 200)
+
+  def test_url_available_by_name(self):
+    response = self.client.get(reverse("listika"))
+    self.assertEqual(response.status_code, 200)
+
+  def test_template_name_correct(self):
+    response = self.client.get(reverse("listika"))
+    self.assertTemplateUsed(response, "listika.html")
+ 
+  def test_template_content(self):
+    response = self.client.get(reverse('listika'))
+    self.assertContains(response, "This is a test!")
