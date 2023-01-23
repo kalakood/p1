4c4e40f7af89d6622503e66c02ffb026fa3d4e4e
Kalakood <>
2023-01-23 22:17:27 +0200

    .

--- app/posts/tests.py
+++ app/posts/tests.py
@@ -15,17 +15,11 @@ class PostTetst(TestCase):
     self.assertEqual(self.post.text, "This is a test!")
 
   def test_url_exists_at_correct_location(self):
-    response = self.client.get(reverse("listika"))
+    response = self.client.get("/") # in module range!
     self.assertEqual(response.status_code, 200)
 
   def test_url_available_by_name(self):
     response = self.client.get(reverse("listika"))
     self.assertEqual(response.status_code, 200)
-
-  def test_template_name_correct(self):
-    response = self.client.get(reverse("listika"))
     self.assertTemplateUsed(response, "listika.html")
- 
-  def test_template_content(self):
-    response = self.client.get(reverse('listika'))
     self.assertContains(response, "This is a test!")
